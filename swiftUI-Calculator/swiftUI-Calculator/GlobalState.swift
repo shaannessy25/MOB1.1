1c658f6b3e032586f68bab51aa2103cf5b838e4a
shaannessy25 <shaan.hurley25@gmail.com>
2021-02-08 17:11:41 -0800

    all operators work

--- swiftUI-Calculator/swiftUI-Calculator/GlobalState.swift
+++ swiftUI-Calculator/swiftUI-Calculator/GlobalState.swift
@@ -11,14 +11,35 @@ import Foundation
 class GlobalState: ObservableObject {
     @Published var display = "0"
     
+    var storedValue: Double? = nil
+    var operation: CalculatorKey? = nil
+    var beginInput: Bool = true
+    
     func keyPressed(key: CalculatorKey) {
         switch key {
         case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
-            if display == CalculatorKey.zero.rawValue {
+            if beginInput {
                 display = key.rawValue
+                beginInput = false
             } else {
                 display = display + key.rawValue
             }
+            
+        case .plus, .minus, .multiply, .divide:
+            if storedValue == nil {
+                storedValue = Double(display)
+                operation = key
+                beginInput = true
+            } else {
+                if beginInput {
+                    operation = key
+                } else {
+                    calculate()
+                    beginInput = true
+                    operation = key
+                }
+            }
+                
         case .dot:
             if display.contains(CalculatorKey.dot.rawValue) {
                 break
@@ -39,5 +60,26 @@ class GlobalState: ObservableObject {
         default:
             break
         }
+    }
+    
+    func calculate() {
+        let a = storedValue ?? 0
+        let b = Double(display) ?? 0
+        var answer: Double = 0
+        
+        switch operation {
+        case .plus:
+            answer = a + b
+        case .minus:
+            answer = a - b
+        case .multiply:
+            answer = a * b
+        case .divide:
+            answer = a / b
+        default:
+            break
+        }
+        storedValue = answer
+        display = String(answer)
     }
 }
